0f5b74a8451f0e175d40e854ad5d6692c5c0ee29
Bar Hofesh <bar.hofesh@gmail.com>
2025-04-22 14:06:32 +0300

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/post-api-render.test.ts
+++ .brightsec/tests/post-api-render.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('POST /api/render', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['ssti', 'secret_tokens', 'unvalidated_redirect', 'xxe', 'osi'],
+      tests: ['ssti'],
       attackParamLocations: [AttackParamLocation.BODY]
     })
     .threshold(Severity.CRITICAL)
